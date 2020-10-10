d94a51b0dc8f0b8f746ce9e0eea04a0bef5a5f74
Nicify <nsnull@protonmail.com>
2020-10-10 15:24:46 +0800

    Build: update build scripts

--- build_win.sh
+++ build_win.sh
@@ -15,7 +15,7 @@ echo "
     </application>
 </assembly>
 " > nvtool.exe.manifest
-
+packr2
 rsrc -manifest nvtool.exe.manifest -ico ./assets/icon.ico -arch amd64 -o rsrc.syso
 
 go build -ldflags='-s -w -H windowsgui -linkmode external -extldflags -static' .
